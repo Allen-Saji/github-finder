ff36d061dbb4e4f9e4a2cd70a80ad8d46bf3aa71
Allen Saji <83496985+Allen-Saji@users.noreply.github.com>
2022-09-27 18:49:56 +0530

    Update About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      Github Finder App can be used to search for github profiles and have look at the profile stats in deatail. This project was made as a practice to familiarize myself with react hooks, tailwind css and github api. 
+      Github Finder App can be used to search for github profiles and have look at the profile stats in detail. This project was made as a practice to familiarize myself with react hooks, tailwind css and github api. 
     </div>
   )
 }
